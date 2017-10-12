7ff65ceab7c497e4c923a29ddb1ffa76a9c562c1
Evandro Murilo <evandromurilob@hotmail.com>
2017-10-12 19:22:51 -0400

    muda rota home para auditoria.index

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AuditoriumController@index')->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('requests', 'RequestController');
 
