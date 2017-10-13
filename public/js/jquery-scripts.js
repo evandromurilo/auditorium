39e88a6d2d95be7fd1f675d8c37f4d1f025dd85a
valdeir <valdeircesconeto@gmail.com>
2017-10-13 01:24:08 -0300

    input de data pronto

---
+++ public/js/jquery-scripts.js
@@ -0,0 +1,5 @@
+/*script máscara do input date*/
+jQuery(function($){
+  $('#date').mask('00/00/0000');
+});
+/*end máscara*/
--- resources/views/auditorium/index.blade.php
+++ resources/views/auditorium/index.blade.php
@@ -3,13 +3,32 @@
 @section('title', 'Auditórios')
 
 @section('content')
-	<h1>Auditórios</h1>
-	<form method="GET" action="{{ route('auditoria.index') }}">
-		<input type="text" name="date" value="{{ $date->toDateString() }}">
-		<input type="submit">
-	</form>
-	<p>{{ $date }}</p>
-	
+
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery.mask.min.js"></script>
+	<script type="text/javascript" src="js/jquery.mask.js"></script>
+	<script type="text/javascript" src="js/jquery-scripts.js"></script>
+
+	<link rel="stylesheet" href="{{ asset('css/style-auditorium-index.css')}}">
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<form class="form-group position-form" method="GET" action="{{ route('auditoria.index') }}">
+					<input type="text" id="date" name="date" class="form-control text-center input-date" autocomplete="off" value="{{-- $date->toDateString() --}}"><br />
+					<input type="submit" class="btn btn-primary position-submit" value="Solicitar Auditório">
+				</form>
+			</div>
+		</div>
+	</div>
+
+
+
+
 	@foreach ($auditoria as $aud)
 		<h2>{{ $aud->name }}</h2>
 		<p>{{ $aud->statusOn($date) }}</p>
