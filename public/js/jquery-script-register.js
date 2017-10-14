db10a16af0df5c978285ccb7192a771692f86d56
Evandro Murilo <evandromurilob@hotmail.com>
2017-10-14 16:43:08 -0400

    atualiza script de cor

--- public/js/jquery-script-register.js
+++ public/js/jquery-script-register.js
@@ -2,11 +2,22 @@ jQuery(function($){
   $('#cel').mask('(99) 99999-9999');
 });
 
-
-
-jQuery(function($) {
+function updateColor() {
   var content = $('#color').val();
   $('#cor').css('background-color', content);
+}
+
+jQuery(function($) {
+	updateColor();
+});
+
+$("body").on('input', '#color', function() {
+	updateColor();
+});
+
+$("body").on('click', '#cor', function() {
+  $("#color").val(randomColor());
+	updateColor();
 });
 
 jQuery(function($) {
