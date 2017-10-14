44ffca94097d8bbe39e6c048f0fa5e4735b121bc
valdeir <valdeircesconeto@gmail.com>
2017-10-14 18:03:22 -0300

    user-show

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -5,17 +5,26 @@
 @section('content')
 
 	<link rel="stylesheet" href="{{ asset('css/style-user-show.css')}}">
+
+
 	<div class="container">
-		<h1>{{ $user->name }}</h1>
-		<div class="perfil-circle" style="background-color:{{ $user->color }};">
-			<div class="letra-perfil">
-				<spam>{{ $user->name[0] }}</spam>
+		<div class="row">
+			<div class="col-md-4 col-lg-4">
+				<h1>{{ $user->name }}</h1>
+				<div class="well" style="background-color:{{ $user->color }};">
+						<p class="letra-perfil text-center">{{ $user->name[0] }}</p>
+				</div>
+
+				<p>{{ $user->description }}</p>
+				<p>{{ $user->email }}</p>
+				<p>{{ $user->cel }}</p>
+				<p class="chat">Chat <i class="fa fa-comments-o" aria-hidden="true"></i></p>
+			</div>
+
+			<div class="col-md-8 col-lg-8">
+				aaaaaaaaa
 			</div>
 		</div>
-
-		<p>{{ $user->description }}</p>
-		<p>{{ $user->email }}</p>
-		<p>{{ $user->cel }}</p>
 	</div>
 
 @endsection
