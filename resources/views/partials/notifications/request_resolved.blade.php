d5d3ebc98993571ef178dcee68a131a4bd5b11b1
Evandro Murilo <evandromurilob@hotmail.com>
2017-10-15 01:13:27 -0400

    add horário da notificação

--- resources/views/partials/notifications/request_resolved.blade.php
+++ resources/views/partials/notifications/request_resolved.blade.php
@@ -1,4 +1,7 @@
 <?php $request = \App\Request::find($notification->data['request_id']) ?>
 <li><a onclick="markRequestNotificationAsRead({{$request->id}})"
 			 href="{{ route('requests.show', ['id'=>$request->id, 'from' => 'notification']) }}">
-			 Sua requisição do {{ $request->auditorium->name }} mudou de status.</a></li>
+			 <div>Sua requisição do {{ $request->auditorium->name }} mudou de status.<br>
+			 <span style="color:gray; font-size: 0.8em;">{{ $notification->created_at }}</span>
+			 </div></a>
+</li>
