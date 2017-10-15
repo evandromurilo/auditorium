e981e15119985fff908e2189682ec94442fa706f
valdeir <valdeircesconeto@gmail.com>
2017-10-15 18:55:14 -0200

    notification restaurada

--- resources/views/partials/notifications/request_resolved.blade.php
+++ resources/views/partials/notifications/request_resolved.blade.php
@@ -1,10 +1,11 @@
 <link rel="stylesheet" href="{{ asset('css/style-norificaton-request-resolver.css') }}">
 
 <?php $request = \App\Request::find($notification->data['request_id']) ?>
-<li>
+
+<li class="erro">
 	<a onclick="markRequestNotificationAsRead({{$request->id}})"
 			 href="{{ route('requests.show', ['id'=>$request->id, 'from' => 'notification']) }}">
-			 <div class="text-justify notification well" style="height: 55px;">Sua requisição do {{ $request->auditorium->name }} mudou de status.<br>
+			 <div class="text-justify notification" style="height: 45px;">Sua requisição do {{ $request->auditorium->name }} mudou de status.<br>
 				 <span class="date-hora">
 					  <i class="fa fa-clock-o" aria-hidden="true"></i> {{ $notification->created_at }}
 					</span>
