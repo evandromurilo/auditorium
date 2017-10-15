8d3575f29610501dca66387fb241fe48504737da
valdeir <valdeircesconeto@gmail.com>
2017-10-15 17:54:41 -0200

    notification ok para google chrome

--- resources/views/partials/notifications/request_resolved.blade.php
+++ resources/views/partials/notifications/request_resolved.blade.php
@@ -1,10 +1,13 @@
 <link rel="stylesheet" href="{{ asset('css/style-norificaton-request-resolver.css') }}">
 
 <?php $request = \App\Request::find($notification->data['request_id']) ?>
-<li><a onclick="markRequestNotificationAsRead({{$request->id}})"
+<li>
+	<a onclick="markRequestNotificationAsRead({{$request->id}})"
 			 href="{{ route('requests.show', ['id'=>$request->id, 'from' => 'notification']) }}">
-			 <div class="text-justify">Sua requisição do {{ $request->auditorium->name }} mudou de status.<br>
-			 <span style="color:gray; font-size: 0.8em; float:right; font-family:'Yantramanav', sans-serif; margin-bottom: 10px;">
-				  <i class="fa fa-clock-o" aria-hidden="true"></i> {{ $notification->created_at }}</span>
-			 </div></a>
+			 <div class="text-justify notification well" style="height: 55px;">Sua requisição do {{ $request->auditorium->name }} mudou de status.<br>
+				 <span class="date-hora">
+					  <i class="fa fa-clock-o" aria-hidden="true"></i> {{ $notification->created_at }}
+					</span>
+			 </div>
+		 </a>
 </li>
