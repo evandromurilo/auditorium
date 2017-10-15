6b10adc524a06515fbddbc2ec869d94121fe6fff
Evandro Murilo <evandromurilob@hotmail.com>
2017-10-15 01:18:55 -0400

    add if no status do request.show

--- resources/views/request/show.blade.php
+++ resources/views/request/show.blade.php
@@ -8,6 +8,14 @@
 			<p><strong>Local:</strong> {{ $request->auditorium->name }}</p>
 			<p><strong>Descrição:</strong> {{ $request->description }}</p>
 			<p><strong>Organizador:</strong> {{ $request->user->name }}</p>
-			<p><strong>Status:</strong> {{ $request->statusF }}</p>
+			<p><strong>Status:</strong> 
+			@if ($request->status == 0)
+				pendente
+			@elseif ($request->status == 1)
+				rejeitado
+			@elseif ($request->status == 2)
+				aceito
+			@endif
+			</p>
 		</div>
 @endsection
