3ce1b60832ea91797922c004661e356f1b38a638
Evandro Murilo <evandromurilob@hotmail.com>
2017-10-16 15:02:54 -0400

    add tooltip e link no botão indisponível

--- resources/views/partials/status.blade.php
+++ resources/views/partials/status.blade.php
@@ -6,5 +6,19 @@
 @elseif ($code == 0)
 	<p class="pendente">Pendente <i class="fa fa-clock-o" aria-hidden="true"></i></p><br>
 @elseif ($code == 2)
-	<p class="indisponivel">Indisponível <i class="fa fa-lock" aria-hidden="true"></i></p><br>
+	@if ($period_code == 0)
+		<?php $first = $statusOn->morning_requests->first() ?>
+	@elseif ($period_code == 1)
+		<?php $first = $statusOn->afternoon_requests->first() ?>
+	@elseif ($period_code == 2)
+		<?php $first = $statusOn->night_requests->first() ?>
+	@endif
+		<a href={{ route('requests.show', $first->id) }}>
+			<p class="indisponivel" data-toggle="tooltip" data-placement="top"
+				 title="{{ $first->event }}">Indisponível
+				 <i class="fa fa-lock" aria-hidden="true"></i>
+			</p>
+		</a><br>
 @endif
+
+
