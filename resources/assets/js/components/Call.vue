42841fe6cf12da7a728e92aa4d6a81c3e524bbe9
valdeir <valdeircesconeto@gmail.com>
2017-10-17 02:35:30 -0300

    alteração no calls

--- resources/assets/js/components/Call.vue
+++ resources/assets/js/components/Call.vue
@@ -1,45 +1,72 @@
+
+
 <template>
-	<div>
-		<h1>{{ call.title }}</h1>
-		<div>
-			<ul class="messages">
-				<call-message v-for="message in n_messages" :message="message"></call-message>
-			</ul>
-		</div>
-		<form method="POST" :action="post_url">
-			<input type="hidden" name="_method" value="post">
-			<input type="hidden" name="_token" :value="csrf_token">
-			<input type="hidden" name="call_id" :value="call.id">
-			<input type="text" name="body">
-			<input type="submit">
-		</form>
-	</div>
+
+<div>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h1>{{ call.title }}</h1>
+      </div>
+
+      <div class="col-md-8 well">
+        <ul class="messages">
+          <call-message v-for="message in n_messages" :message="message"></call-message>
+        </ul>
+
+
+      <form method="POST" :action="post_url">
+        <input type="hidden" name="_method" value="post">
+        <input type="hidden" name="_token" :value="csrf_token">
+        <input type="hidden" name="call_id" :value="call.id">
+				<div class="form-group">
+					<input class="form-control" type="text" name="body">
+				</div>
+				<div class="form-group">
+					 <input class="btn btn-primary" type="submit">
+				</div>
+
+      </form>
+			</div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-	props:['call', 'messages', 'members'],
-	data() {
-		return {
-			post_url: "/messages",
-			csrf_token: $('meta[name=csrf-token]').attr('content'),
-			members_lookup: {},
-			n_messages: [],
-		}
-	},
-	mounted() {
-		console.log('Calls: Component mounted.');
+  props: ['call', 'messages', 'members'],
+  data() {
+    return {
+      post_url: "/messages",
+      csrf_token: $('meta[name=csrf-token]').attr('content'),
+      members_lookup: {},
+      n_messages: [],
+    }
+  },
+  mounted() {
+    console.log('Calls: Component mounted.');
 
-		for (var i = 0, len = this.members.length; i < len; i++) {
-			this.members_lookup[this.members[i].id] = this.members[i];
-		}
+    for (var i = 0, len = this.members.length; i < len; i++) {
+      this.members_lookup[this.members[i].id] = this.members[i];
+    }
 
-		for (var i = 0, len = this.messages.length; i < len; i++) {
-			this.messages[i].author = this.members_lookup[this.messages[i].user_id];
-			this.n_messages.push(this.messages[i]);
-		}
+    for (var i = 0, len = this.messages.length; i < len; i++) {
+      this.messages[i].author = this.members_lookup[this.messages[i].user_id];
+      this.n_messages.push(this.messages[i]);
+    }
 
-		console.log(this.messages);
-	}
+    console.log(this.messages);
+  }
 }
 </script>
+
+
+<style>
+h1{
+	color: green;
+}
+
+</style>
