1eab2a8355fd85b6725359c7e8a7d4ed0180c69b
Evandro Murilo <evandromurilob@hotmail.com>
2017-10-18 15:02:19 -0400

    add call.index

---
+++ resources/views/call/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Chamadas')
+
+@section('content')
+	<h1>Chamadas</h1>
+	<ul>
+	@foreach ($calls as $call)
+		<li><a href="{{ route('calls.show', $call->id) }}">
+			{{ $call->title }}
+		</a></li>
+	@endforeach
+	</ul>
+@endsection
