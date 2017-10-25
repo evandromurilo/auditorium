36d768c160fda1c2fb571cb6f4eec4f165fc3d89
valdeir <valdeircesconeto@gmail.com>
2017-10-25 10:53:22 -0300

    pagina de erros pronto

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,6 +3,13 @@
 @section('title', '403 ERROR')
 
 @section('content')
-	<h1>403 ERROR</h1>
-	<p>Você não tem permissão para acessar esta página, danadinho!</p>
+	<div class="position">
+		<h1 class="text-center text-erro">403 ERROR 😑</h1>
+		<p class="text-center text">Você não tem permissão para acessar esta página, danadinho!</p>
+	</div>
+
+@endsection
+
+@section('sources')
+<link rel="stylesheet" href="{{ asset('css/style-erros.css')}}">
 @endsection
