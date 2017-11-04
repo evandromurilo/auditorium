4cb59a1428f117085e17cd85a0c9c68e48cb3725
Evandro Murilo <evandromurilob@hotmail.com>
2017-11-04 14:36:38 -0400

    cria chat geral

---
+++ app/Listeners/AssignNewUserToChat.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AssignNewUserToChat
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserRegistered  $event
+     * @return void
+     */
+    public function handle(UserRegistered $event)
+    {
+			$call = \App\Call::find(1);
+
+			$call->members()->attach($event->user->id);
+			$event->user->allow('see', $call);
+    }
+}
