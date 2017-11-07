aff3acb972380e6ba05a207484b3dab5c7758de6
valdeir <valdeircesconeto@gmail.com>
2017-11-07 01:42:49 -0300

    alteração na estrutura do css de alguns arquivos

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,19 +9,23 @@
 		</div>
 	</div>
 	<div class="container">
-		<div class="">
-			<div class="">
+		<div class="row">
+				@foreach ($users as $user)
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+						<div class="well" style="border: 1px solid {{ $user->color }};">
+							<a class="user" href="{{ route('users.show', $user->id) }}">
+								{{ $user->name }}
+							</a>
 
-			</div>
+							<div class="row">
+								<label class="col-md-4 control-label">Cargo: </label>
+								<span class="col-md-8">{{ $user->description}}</span>
+							</div>
+
+						</div>
+					</div>
+				@endforeach
 		</div>
-		@foreach ($users as $user)
-
-				<a style="color: {{ $user->color }};" href="{{ route('users.show', $user->id) }}">
-					{{ $user->name }}
-				</a>
-
-		@endforeach
-
 	</div>
 
 @endsection
