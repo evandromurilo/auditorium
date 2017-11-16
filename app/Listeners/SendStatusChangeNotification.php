4e3948abda7022d94862916b08f9ddffced617c3
Evandro Murilo <evandromurilob@hotmail.com>
2017-11-16 12:48:18 -0400

    only one notification for request

--- app/Listeners/SendStatusChangeNotification.php
+++ app/Listeners/SendStatusChangeNotification.php
@@ -28,6 +28,19 @@ class SendStatusChangeNotification
     public function handle(RequestStatusChanged $event)
     {
         \Log::info('notification', ['request' => $event->request]);
-				$event->request->user->notify(new RequestResolved($event->request));
+
+				$send = true;
+
+				foreach ($event->request->user->unreadNotifications as $notification) {
+					if ($notification->type == "App\Notifications\RequestResolved" &&
+						$notification->data['request_id'] == $event->request->id) {
+						$send = false;
+						break;
+					}
+				}
+
+				if ($send) {
+					$event->request->user->notify(new RequestResolved($event->request));
+				}
     }
 }
