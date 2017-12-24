03d37b82667da0068e9d4fb01c99f7e747246856
valdeir <valdeircesconeto@gmail.com>
2017-12-24 13:41:31 -0300

    status removido do progress_bar

--- resources/views/partials/progress_bar.blade.php
+++ resources/views/partials/progress_bar.blade.php
@@ -1,13 +1,16 @@
 @if ($code == 0)
-<div class="progress-bar"  style="width: 14.286%; background-color: #FF8C00;"  data-toggle="tooltip" data-placement="top" title="Pendente">{{ $period }}
+<div class="progress-bar"  style="width: 14.286%; background-color: #FF8C00;"  data-toggle="tooltip" data-placement="top" title="Pendente">
 	<span class="sr-only">Pendente para a {{ $period }}.</span>
 </div>
 @elseif ($code == 1)
-<div class="progress-bar"  style="width: 14.286%; background-color: green;"  data-toggle="tooltip" data-placement="top" title="Disponível">{{ $period }}
+<div class="progress-bar"  style="width: 14.286%; background-color: green;"  data-toggle="tooltip" data-placement="top" title="Disponível">
 	<span class="sr-only">Disponível para a {{ $period }}.</span>
 </div>
 @else
-<div class="progress-bar"  style="width: 14.286%; background-color: red;" data-toggle="tooltip" data-placement="top" title="Indisponível">{{ $period }}
+<div class="progress-bar"  style="width: 14.286%; background-color: red;" data-toggle="tooltip" data-placement="top" title="Indisponível">
 	<span class="sr-only">Indisponível para a {{ $period }}.</span>
 </div>
 @endif
+
+
+<!-- tirei todos os {{--$period--}} para não aparecer mais no front caso precisar so colocar novamente*/-->
