7fb5287badc6a9a5574d413439a2ad1addee3fa8
valdeir <valdeircesconeto@gmail.com>
2018-01-08 13:26:36 -0300

    script auditorio

--- public/js/script-auditorium-2.0.js
+++ public/js/script-auditorium-2.0.js
@@ -4,11 +4,9 @@ $(document).ready(function() {
   $('.icone-down').click(function(){
     $(this).find('i').toggleClass('fa fa-caret-up fa fa-caret-down');
   });
-
-
-/*falha no script*/
-/*
-  $('.icone-down').click(function(){
-    $('.status-periodo').hide('slow');
-  });
 });
+/*
+
+$('.icone-down').click(function(){
+  $('.status-periodo').hide('slow');
+});*/
