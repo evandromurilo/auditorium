93d25494105ca3b2b68c3d60b8b379fc2b88d14d
Evandro Murilo <evandromurilob@hotmail.com>
2018-01-17 20:31:21 -0400

    add auth middleware to AuditoriumController

--- app/Http/Controllers/AuditoriumController.php
+++ app/Http/Controllers/AuditoriumController.php
@@ -7,6 +7,10 @@ use Carbon\Carbon;
 use \App\Auditorium;
 
 class AuditoriumController extends Controller {
+	public function __construct() {
+		$this->middleware('auth');
+	}
+
 	public function index(Request $request) {
 		if ($request->has('date')) {
 			$date = Carbon::createFromFormat('d/m/Y', $request->date);
