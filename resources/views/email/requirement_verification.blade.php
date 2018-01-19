d907f705df5b76ebbd59a6cd439f64e25317ad0d
valdeir <valdeircesconeto@gmail.com>
2018-01-19 11:48:28 -0300

    css email do servidor.

--- resources/views/email/requirement_verification.blade.php
+++ resources/views/email/requirement_verification.blade.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="{{asset('css/style-requirement-verification.css')}}">
 <style media="screen">
 .button{
     background-color: #2a88bd;
@@ -16,10 +15,10 @@
 		}
     p{
 			font-size: 15px;
-      color: #777;
+      color: #000;
 		}
     h1{
-      color:#777;
+      color:#000;
     }
     .btn-confirmation{
       display: flex;
@@ -32,18 +31,29 @@
     a, p, h1{
       font-family: 'Lato', sans-serif;
     }
+    .style-information, .style-variavel{
+      display: inline;
+      color: #000;
+    }
+    .style-information{
+      color:#fff;
+			border: 1px solid #777;
+			border-radius: 5px;
+			padding: 4px 10px 4px 10px;
+			background-color: #777;
+    }
 </style>
-<h1 class="title">{{ $requirement->name }}</h1><p class="title">,precisam da sua presença!</p><br><br>
-<p>Evento: {{ $requirement->request->event }}</p>
-<p>Data: {{ $requirement->request->dateC->format('d/m/Y') }}</p><i class="fa fa-calendar" aria-hidden="true"></i>
-<p>Horário: {{ $requirement->request->periodTimeF }}</p>
-<p>Local: {{ $requirement->request->auditorium->name }}</p>
 
+<h1 class="title">{{ $requirement->name }}</h1><p class="title">,precisam da sua presença!</p><br><br><br>
+<p class="style-variavel">Evento: </p><p class="style-information"> {{ $requirement->request->event }}</p><br><br>
+<p class="style-variavel">Data: </p><p class="style-information">{{ $requirement->request->dateC->format('d/m/Y') }}</p><br><br>
+<p class="style-variavel">Horário: </p><p class="style-information">{{ $requirement->request->periodTimeF }}</p><br><br>
+<p class="style-variavel">Local: </p><p class="style-information">{{ $requirement->request->auditorium->name }}</p><br><br><br>
+
+<!--button confirmar-->
 <div class="btn-confirmation">
-
     <a class="button" href="{{ route('requirements.updateVerification',
-      ['id' => $requirement->id, 'hash' => $verification->hash]) }}" class="btn btn-success btn-lg">
+      ['id' => $requirement->id, 'hash' => $verification->hash]) }}">
       Clique para confirmar a presença
     </a>
-
 </div>
