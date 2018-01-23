5d827c5ac573aaba9d7fe08f1a2c408b1ddc61d7
valdeir <valdeircesconeto@gmail.com>
2018-01-23 09:07:23 -0300

    requirementes

--- resources/views/requirement/verification.blade.php
+++ resources/views/requirement/verification.blade.php
@@ -3,20 +3,32 @@
 @section('title', 'Verificação')
 
 @section('content')
-  <h1 id="verification-title">Responder à requisição</h1>
-  <p>Evento: {{ $requirement->request->event }}</p>
-  <p>Data: {{ $requirement->request->dateC->format('d/m/Y') }}</p>
-  <p>Horário: {{ $requirement->request->periodTimeF }}</p>
-  <p>Local: {{ $requirement->request->auditorium->name }}</p>
-  <button class="btn btn-primary"
-          id="btn-confirm">
-        Estarei presente
-  </button>
-  <button class="btn btn-primary"
-          id="btn-negate">
-        Não poderei comparecer
-  </button>
-  <div id="response" style="color: green;"></div>
+  <link rel="stylesheet" href="{{ asset('css/style-requirements-verification.css')}}">
+
+  <div class="container">
+    <div class="row">
+			<div class="col-sm-12 col-md-12 col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">Confirmação da Presença</div>
+          <h1 id="verification-title" class="text-center">Responder à requisição</h1>
+          <p>Evento: {{ $requirement->request->event }}</p>
+          <p>Data: {{ $requirement->request->dateC->format('d/m/Y') }}</p>
+          <p>Horário: {{ $requirement->request->periodTimeF }}</p>
+          <p>Local: {{ $requirement->request->auditorium->name }}</p>
+          <button class="btn btn-primary btn-lg"
+                  id="btn-confirm">
+                Estarei presente
+          </button>
+          <button class="btn btn-primary btn-lg"
+                  id="btn-negate">
+                Não poderei comparecer
+          </button>
+          <div id="response" style="color: green;"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 @endsection
 
 <script type="text/javascript" src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
