80b558439d5a9c5287864539dfac5bda996715aa
Evandro Murilo <evandromurilob@hotmail.com>
2018-01-25 19:49:03 -0400

    commit to test a thing in server

--- app/Http/Controllers/AuditoriumController.php
+++ app/Http/Controllers/AuditoriumController.php
@@ -15,7 +15,7 @@ class AuditoriumController extends Controller {
 		if ($request->has('date')) {
 			$date = Carbon::createFromFormat('d/m/Y', $request->date);
 		} else {
-			$date = Carbon::today();
+			$date = Carbon::now();
 		}
 
 		if ($request->has('next')) {
