a356900cec53d9a675d6d92078609720a1559f42
Evandro Murilo <evandromurilob@hotmail.com>
2018-01-27 19:44:46 -0400

    translate bottom of email template

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -51,8 +51,8 @@ Atenciosamente,<br>{{ config('app.name') }}
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+Se estiver tendo problemas para clicar no botão "{{ $actionText }}", copie e cole a URL abaixo no
+seu navegador: [{{ $actionUrl }}]({{ $actionUrl }})
 @endcomponent
 @endisset
 @endcomponent
